e2620c22dd7a23b66a3056c71d99ac4dbde26034
niraj876 <nrjvivek876@gmail.com>
2022-03-08 15:07:29 +0530

    first commit

---
+++ webapp/serializers.py
@@ -0,0 +1,10 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import employees
+
+
+class employeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = employees
+        # fields = ('firstname', 'lastname')
+        fields = '__all__'
